82af38a6b06f3d42a0ec1e1ca5e582d6875561ad
okamis <henningphan@hotmail.com>
2014-11-10 17:29:53 +1100

    Heuristic wells added

--- src/Heuristic/Wells.java
+++ src/Heuristic/Wells.java
@@ -6,10 +6,29 @@ public class Wells extends Heuristic{
 
 	@Override
 	public int calculate(TetrisMap gm) {
+		int count = 0;
 		for(int i=0;i<gm.map.size(); ++i){
-			
+			int size = gm.map.get(i).size();
+			// First case
+			if( i==0){
+				int sizeNext = gm.map.get(i+1).size();
+				count+= size <sizeNext? sizeNext-size: 0;
+				continue;
+			}
+			// Last case
+			if( i==gm.map.size()-1){
+				int sizePrev = gm.map.get(i-1).size();
+				count+= size <sizePrev? sizePrev-size: 0;
+				continue;
+			}
+			int sizeNext = gm.map.get(i+1).size();
+			int sizePrev = gm.map.get(i-1).size();
+			if( size < sizeNext && size < sizePrev){
+				int min = Math.min(sizeNext, sizePrev);
+				count += min -size;
+			}
 		}
-		return 0;
+		return count*weight;
 	}
 
 }
